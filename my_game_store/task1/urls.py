c8e6ca5c487a30caf30e96a33a5bc45bfee8ce75
MixDream <max_juldashev@mail.ru>
2024-12-08 19:11:35 +0300

    init

--- my_game_store/task1/urls.py
+++ my_game_store/task1/urls.py
@@ -7,3 +7,13 @@ urlpatterns = [
     path('cart/', cart, name='cart'),
     path('registration/', registration, name='registration'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.create_posts, name='create_posts'),
+    path('update/<int:post_id>/', views.update_post, name='update_post'),
+    path('list/', views.list_posts, name='list_posts'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('filter/', views.filter_posts, name='filter_posts'),
+]
--- my_game_store/task1/views.py
+++ my_game_store/task1/views.py
@@ -19,3 +19,26 @@ def registration(request):
             Buyer.objects.create(name=username, balance=0.0, age=18)
             return redirect('index')
     return render(request, 'registration.html')
+from django.shortcuts import render
+from .models import Post
+
+def create_posts(request):
+    Post.objects.create(title='Первая статья', content='Содержание первой статьи', published_date='2024-01-01')
+    Post.objects.create(title='Вторая статья', content='Содержание второй статьи', published_date='2024-02-01')
+    Post.objects.create(title='Третья статья', content='Содержание третьей статьи', published_date='2024-03-01')
+    return render(request, 'task1/posts_created.html')
+def update_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.title = 'Новый заголовок'
+    post.save()
+    return render(request, 'task1/post_updated.html', {'post': post})
+def list_posts(request):
+    all_posts = Post.objects.all()
+    return render(request, 'task1/list_posts.html', {'posts': all_posts})
+def delete_post(request, post_id):
+    post_to_delete = Post.objects.get(id=post_id)
+    post_to_delete.delete()
+    return render(request, 'task1/post_deleted.html')
+def filter_posts(request):
+    posts_2024 = Post.objects.filter(published_date__year=2024)
+    return render(request, 'task1/filter_posts.html', {'posts': posts_2024})
